c6c4ff7903c6bba58114196f3e227b6cb84c5b60
AzizTeqnite123 <azizteqnite@gmail.com>
2025-02-28 23:16:23 +0500

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".prodSlider", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".testiSlider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true, initialSlide: 0,loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
